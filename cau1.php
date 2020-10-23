f93d3f0721acb39e64c3f96eeff96e01b28d8988
Tue Nguyen <tue.nguyen@miracles.vn>
2020-10-23 17:36:08 +0700

    updatee

--- cau1.php
+++ cau1.php
@@ -1,13 +1,15 @@
 <?php
 
-
+$output = [];
 $n = 3;
 	$arr = array(11,2,8,10,5,99,1,8,9);
 	for($i = 0; $i < $n; $i++){
 		for($j = 0; $j < count($arr); $j++){
-			if($j % 3 == $i )
-        		echo  $arr[$j] . ', ' ;
+			if($j % $n == $i )
+				// echo  $arr[$j] . ', ' ;
+				array_push($output, $arr[$j]);	
+				
 		}
 	}
-
+	print_r($output);
 ?>
