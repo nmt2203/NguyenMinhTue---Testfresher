f6e93c4db16bee4740e5466e3c2c623e8f9b6bc4
Tue Nguyen <tue.nguyen@miracles.vn>
2020-10-23 17:29:48 +0700

    update

--- cau3.php
+++ cau3.php
@@ -1,15 +1,29 @@
 <?php
-$mang = [];
-$numbers = array(1,5,4,7,9,0,-10,13,93,14,15);
-sort($numbers);
+    $mang = array();
+    $numbers = array(1,5,4,7,9,0,-10,13,93,14,15);
+    sort($numbers);
+  
+    $n  = count($numbers);
 
-$n  = count($numbers);
-for($i = 0; $i < $n;$i++){
-    if($i = $n-1){
-        echo $numbers[$i]-$numbers[$i-1];
+    for($i = 0; $i < $n;$i++){
+        if($i == $n-1){
+            break;
+        }
+        $number_mang = $numbers[$i+1] - $numbers[$i];
+        array_push($mang, $number_mang);
+     
     }
-    else {
-        echo $numbers[$i+1]-$numbers[$i];
+  
+    $min = min($mang);
+    $output = [];
+    for($i = 0; $i < $n;$i++){
+        if($i == $n-1){
+            break;
+        }
+        if($numbers[$i+1] - $numbers[$i] == $min){ 
+            array_push($output, '['. $numbers[$i] . ', ' . $numbers[$i+1]. ']');
+            
+        }
     }
-}
+    print_r($output);
 ?>
